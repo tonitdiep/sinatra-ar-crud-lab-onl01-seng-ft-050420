9c7e19cddd8a69361861302517af856d666ef27f
Toni  Diep <adiep123@gmail.com>
2020-06-26 07:39:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+# remember for later format-wise
 # @article = Article.new(params[:title])
 # @article = Article.create(params)
   get '/' do
@@ -20,12 +20,18 @@ class ApplicationController < Sinatra::Base
   get '/articles/new' do 
     erb :new
   end
+  
+  post '/articles' do 
+    # binding.pry
+    # @article = Article.create(params)
+    @article = Article.create
+    redirect 
+    erb :show 
+    binding.pry
+  end
+  
   get '/articles/:id' do
     @article = params[:id]
     erb :show
   end
-  
-  
-
-  
 end
